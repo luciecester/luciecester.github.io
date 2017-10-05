feb515a49cb0f3fac8b1814ea7dfcc8ff2cdfbf0
luciecester <30708319+luciecester@users.noreply.github.com>
2017-10-06 09:24:00 +1100

    project 2 push

---
+++ project 2/HTMLboilerplate/assets/js/main.js
@@ -0,0 +1,98 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+$('.nav_mouths').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.mouths').removeClass('hide');
+  $('.item.mouths').addClass('show');
+});
+
+$('.nav_eyes').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.eyes').removeClass('hide');
+  $('.item.eyes').addClass('show');
+});
+
+$('.nav_villains').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.villains').removeClass('hide');
+  $('.item.villains').addClass('show');
+});
+
+$('.nav_angry').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.angry').removeClass('hide');
+  $('.item.angry').addClass('show');
+});
+
+$('.nav_fighting').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.fighting').removeClass('hide');
+  $('.item.fighting').addClass('show');
+});
+
+$('.nav_weapons').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.weapons').removeClass('hide');
+  $('.item.weapons').addClass('show');
+});
+
+$('.nav_gore').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.gore').removeClass('hide');
+  $('.item.gore').addClass('show');
+});
+
+$('.nav_gif').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.gif').removeClass('hide');
+  $('.item.gif').addClass('show');
+});
+
+$('.nav_all').click(function(event) {
+  $('.item').removeClass('hide');
+  $('.item').addClass('show');
+
+
+});
+
+
+$('.nav_thumbnail').click(function(event) {
+  $('.item').addClass('thumbnail');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
